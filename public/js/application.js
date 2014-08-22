605f2a5419258e049248d92282c99293bbaf67c3
Ryan Stack <ryanstack@Ryans-MacBook-Pro-2.local>
2014-08-22 13:38:23 -0700

    removed intro and overlay on query.  Added home link

--- public/js/application.js
+++ public/js/application.js
@@ -3,13 +3,69 @@ $(document).ready(function() {
   $('#register_button').on("click", registerDropDown)
   $('form#foodsearch').submit(apiCall)
   $('.recipes').on('click', '.link', apiInstructions)
-
-
 });
 
-var removePopUp = function(event) {
-  $('.popup').empty()
+//All code relating to recipe search
+
+var apiCall = function(event) {
+  event.preventDefault()
+  $('.recipes').empty()
+  var searchfood = $('#ingredients').val().split(' ')[0]
+  var ingredients = $('#ingredients').val().split(' ')
+  console.log (searchfood)
+  console.log (ingredients)
+  var ajaxRequest = $.ajax({
+    url:  '/recipe',
+    type: "GET",
+    data: {q: searchfood, app_id: '9285586a', app_key: 'd28d4c08b01a940f886faacc4a94e00b', allowedIngredient: ingredients},
+    dataType: 'json'
+  })
+  ajaxRequest.success(function(data) {
+      console.log (data)
+      var intro = document.getElementsByClassName('intro')
+      intro[0].style.display = 'none';
+      var overlay = document.getElementsByClassName('overlay')
+      overlay[0].style.display = 'none';
+      if (data.length === 0) {
+        $('.recipes').append($('<p/>', {
+      text: "Hit the Supermarket.  There are no recipes with those ingredients."
+    }))
+      }
+      for (var i=0; i<data.length; i++) {
+        var name = data[i].recipe
+        var ingredients = data[i].ingredients
+        var picture = data[i].picture
+        var id = data[i].id
+        recipeAppender(name, ingredients, picture, id)
+      }
+
+  })
 }
+
+var recipeAppender = function(name, ingredients, picture, id) {
+  var row = buildRecipeRow(name, ingredients, picture, id)
+  $('.recipes').append(row)
+
+}
+
+function buildRecipeRow(name, ingredients, picture, id) {
+  // gets todoTemplate stored in DOM.
+  var recipeTemplate = $.trim($('#recipe_template').html());
+  // Creates an jQueryDOMElement from the todoTemplate.
+  var $recipe = $(recipeTemplate);
+  // Modifies it's text to use the passed in todoName.
+  $recipe.find('img').attr("src", picture)
+  $recipe.find('h4').text(name);
+  $recipe.find('h5').text(ingredients)
+  $recipe.find('.link').data("foodlink", id)
+  // Returns the jQueryDOMElement to be used elsewhere.
+  return $recipe;
+}
+
+
+
+
+//All code relating to Instructions
 
 var apiInstructions = function(event) {
   event.preventDefault()
@@ -36,17 +92,6 @@ var apiInstructions = function(event) {
 })
 }
 
-
-
-var instructionPopUp = function(name, ingredients, yield, time, source) {
-  var popup = buildInstructionPopUp(name, ingredients, yield, time, source)
-  $('.popup').html(popup)
-
-  $('#exit').on("click", removePopUp)
-}
-
-
-
 function buildInstructionPopUp(name, ingredients, yield, time, source) {
   // gets todoTemplate stored in DOM.
   var instructionTemplate = $.trim($('#instruction_template').html());
@@ -68,14 +113,20 @@ function buildInstructionPopUp(name, ingredients, yield, time, source) {
   return $instruction;
 }
 
+var instructionPopUp = function(name, ingredients, yield, time, source) {
+  var popup = buildInstructionPopUp(name, ingredients, yield, time, source)
+  $('.popup').html(popup)
+
+  $('#exit').on("click", removePopUp)
+}
+
+var removePopUp = function(event) {
+  $('.popup').empty()
+}
 
 
 
-
-
-
-
-
+//All code relating to Login/Register
 var signInDropDown = function(event) {
   form = $('#login').html()
   $('#content').html(form)
@@ -85,56 +136,3 @@ var registerDropDown = function(event) {
   form = $('#register').html()
   $('#content').html(form)
 }
-
-var apiCall = function(event) {
-  event.preventDefault()
-  $('.recipes').empty()
-  var searchfood = $('#ingredients').val().split(' ')[0]
-  var ingredients = $('#ingredients').val().split(' ')
-  console.log (searchfood)
-  console.log (ingredients)
-  var ajaxRequest = $.ajax({
-    url:  '/recipe',
-    type: "GET",
-    data: {q: searchfood, app_id: '9285586a', app_key: 'd28d4c08b01a940f886faacc4a94e00b', allowedIngredient: ingredients},
-    dataType: 'json'
-  })
-  ajaxRequest.success(function(data) {
-      console.log (data)
-      if (data.length === 0) {
-        $('.recipes').append($('<p/>', {
-      text: "Hit the Supermarket.  There are no recipes with those ingredients."
-    }))
-      }
-      for (var i=0; i<data.length; i++) {
-        var name = data[i].recipe
-        var ingredients = data[i].ingredients
-        var picture = data[i].picture
-        var id = data[i].id
-        recipeAppender(name, ingredients, picture, id)
-      }
-      // console.log(data)
-      // $('.recipes').html(data)
-  })
-}
-
-var recipeAppender = function(name, ingredients, picture, id) {
-  var row = buildRecipeRow(name, ingredients, picture, id)
-  $('.recipes').append(row)
-
-}
-
-function buildRecipeRow(name, ingredients, picture, id) {
-  // gets todoTemplate stored in DOM.
-  var recipeTemplate = $.trim($('#recipe_template').html());
-  // Creates an jQueryDOMElement from the todoTemplate.
-  var $recipe = $(recipeTemplate);
-  // Modifies it's text to use the passed in todoName.
-  $recipe.find('img').attr("src", picture)
-  $recipe.find('h4').text(name);
-  $recipe.find('h5').text(ingredients)
-  $recipe.find('.link').data("foodlink", id)
-  // Returns the jQueryDOMElement to be used elsewhere.
-  return $recipe;
-}
-
